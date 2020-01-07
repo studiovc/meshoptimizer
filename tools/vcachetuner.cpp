517f9c6dd59cbed3d7c197de8863ce1265ad14e4
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2020-01-06 21:57:36 -0800

    tools/vcachetuner: Restore GA and SA algorithms
    It would be interesting to do a full comparison between GA, SA and DE in
    terms of learning rate and getting stuck in a local minima on the
    compression optimization.

--- tools/vcachetuner.cpp
+++ tools/vcachetuner.cpp
@@ -257,6 +257,91 @@ float fitness_score(const State& state, const std::vector<Mesh>& meshes)
 	return result / count;
 }
 
+size_t rndindex(const std::vector<float>& prob)
+{
+	float r = rand01();
+
+	for (size_t i = 0; i < prob.size(); ++i)
+	{
+		r -= prob[i];
+
+		if (r <= 0)
+			return i;
+	}
+
+	return prob.size() - 1;
+}
+
+State mutate(const State& state)
+{
+	State result = state;
+
+	if (rand01() < 0.7f)
+	{
+		size_t idxcache = std::min(int(rand01() * kCacheSizeMax + 0.5f), int(kCacheSizeMax - 1));
+
+		result.cache[idxcache] = rand01();
+	}
+
+	if (rand01() < 0.7f)
+	{
+		size_t idxlive = std::min(int(rand01() * kValenceMax + 0.5f), int(kValenceMax - 1));
+
+		result.live[idxlive] = rand01();
+	}
+
+	if (rand01() < 0.2f)
+	{
+		uint32_t mask = rand32();
+
+		for (size_t i = 0; i < kCacheSizeMax; ++i)
+			if (mask & (1 << i))
+				result.cache[i] *= 0.9f + 0.2f * rand01();
+	}
+
+	if (rand01() < 0.2f)
+	{
+		uint32_t mask = rand32();
+
+		for (size_t i = 0; i < kValenceMax; ++i)
+			if (mask & (1 << i))
+				result.live[i] *= 0.9f + 0.2f * rand01();
+	}
+
+	if (rand01() < 0.05f)
+	{
+		uint32_t mask = rand32();
+
+		for (size_t i = 0; i < kCacheSizeMax; ++i)
+			if (mask & (1 << i))
+				result.cache[i] = rand01();
+	}
+
+	if (rand01() < 0.05f)
+	{
+		uint32_t mask = rand32();
+
+		for (size_t i = 0; i < kValenceMax; ++i)
+			if (mask & (1 << i))
+				result.live[i] = rand01();
+	}
+
+	return result;
+}
+
+bool accept(float fitnew, float fitold, float temp)
+{
+	if (fitnew >= fitold)
+		return true;
+
+	if (temp == 0)
+		return false;
+
+	float prob = exp2((fitnew - fitold) / temp);
+
+	return rand01() < prob;
+}
+
 std::vector<State> gen0(size_t count, const std::vector<Mesh>& meshes)
 {
 	std::vector<State> result;
@@ -279,9 +364,163 @@ std::vector<State> gen0(size_t count, const std::vector<Mesh>& meshes)
 	return result;
 }
 
+// https://en.wikipedia.org/wiki/Genetic_algorithm
+std::pair<State, float> genN_GA(std::vector<State>& seed, const std::vector<Mesh>& meshes, float crossover, float mutate)
+{
+	std::vector<State> result;
+	result.reserve(seed.size());
+
+	std::vector<float> seedprob(seed.size());
+
+#pragma omp parallel for
+	for (size_t i = 0; i < seed.size(); ++i)
+	{
+		seedprob[i] = fitness_score(seed[i], meshes);
+	}
+
+	State best = {};
+	float bestfit = 0;
+	float probsum = 0;
+
+	for (size_t i = 0; i < seed.size(); ++i)
+	{
+		float score = seedprob[i];
+		probsum += score;
+
+		if (score > bestfit)
+		{
+			best = seed[i];
+			bestfit = score;
+		}
+	}
+
+	for (auto& prob : seedprob)
+	{
+		prob /= probsum;
+	}
+
+	std::vector<unsigned int> seedidx;
+	seedidx.reserve(seed.size());
+	for (size_t i = 0; i < seed.size(); ++i)
+		seedidx.push_back(i);
+
+	std::sort(seedidx.begin(), seedidx.end(), [&](size_t l, size_t r) { return seedprob[l] < seedprob[r]; });
+
+	while (result.size() < seed.size() / 4)
+	{
+		size_t idx = seedidx.back();
+		seedidx.pop_back();
+
+		result.push_back(seed[idx]);
+	}
+
+	while (result.size() < seed.size())
+	{
+		State s0 = seed[rndindex(seedprob)];
+		State s1 = seed[rndindex(seedprob)];
+
+		State state = s0;
+
+		// crossover
+		if (rand01() < crossover)
+		{
+			size_t idxcache = std::min(int(rand01() * kCacheSizeMax + 0.5f), 15);
+
+			memcpy(state.cache + idxcache, s1.cache + idxcache, (kCacheSizeMax - idxcache) * sizeof(float));
+		}
+
+		if (rand01() < crossover)
+		{
+			size_t idxlive = std::min(int(rand01() * kValenceMax + 0.5f), 7);
+
+			memcpy(state.live + idxlive, s1.live + idxlive, (kValenceMax - idxlive) * sizeof(float));
+		}
+
+		// mutate
+		if (rand01() < mutate)
+		{
+			size_t idxcache = std::min(int(rand01() * kCacheSizeMax + 0.5f), 15);
+
+			state.cache[idxcache] = rand01();
+		}
+
+		if (rand01() < mutate)
+		{
+			size_t idxlive = std::min(int(rand01() * kValenceMax + 0.5f), 7);
+
+			state.live[idxlive] = rand01();
+		}
+
+		result.push_back(state);
+	}
+
+	seed.swap(result);
+
+	return std::make_pair(best, bestfit);
+}
+
+// https://en.wikipedia.org/wiki/Simulated_annealing
+std::pair<State, float> genN_SA(std::vector<State>& seed, const std::vector<Mesh>& meshes, size_t steps)
+{
+	std::vector<State> result;
+	result.reserve(seed.size() * (1 + steps));
+
+	// perform several parallel steps of mutation for each temperature
+	for (size_t i = 0; i < seed.size(); ++i)
+	{
+		result.push_back(seed[i]);
+
+		for (size_t s = 0; s < steps; ++s)
+			result.push_back(mutate(seed[i]));
+	}
+
+	// compute fitness for all temperatures & mutations in parallel
+	std::vector<float> resultfit(result.size());
+
+#pragma omp parallel for
+	for (size_t i = 0; i < result.size(); ++i)
+	{
+		resultfit[i] = fitness_score(result[i], meshes);
+	}
+
+	// perform annealing for each temperature
+	std::vector<float> seedfit(seed.size());
+
+	for (size_t i = 0; i < seed.size(); ++i)
+	{
+		size_t offset = i * (1 + steps);
+
+		seedfit[i] = resultfit[offset];
+
+		float temp = (float(i) / float(seed.size() - 1)) / 0.1f;
+
+		for (size_t s = 0; s < steps; ++s)
+		{
+			if (accept(resultfit[offset + s + 1], seedfit[i], temp))
+			{
+				seedfit[i] = resultfit[offset + s + 1];
+				seed[i] = result[offset + s + 1];
+			}
+		}
+	}
+
+	// perform promotion from each temperature to the next one
+	for (size_t i = seed.size() - 1; i > 0; --i)
+	{
+		if (seedfit[i] > seedfit[i - 1])
+		{
+			seedfit[i - 1] = seedfit[i];
+			seed[i - 1] = seed[i];
+			break;
+		}
+	}
+
+	return std::make_pair(seed[0], seedfit[0]);
+}
+
 // https://en.wikipedia.org/wiki/Differential_evolution
 // Good Parameters for Differential Evolution. Magnus Erik Hvass Pedersen, 2010
-std::pair<State, float> genN(std::vector<State>& seed, const std::vector<Mesh>& meshes, float crossover = 0.8803f, float weight = 0.4717f)
+std::pair<State, float> genN_DE(std::vector<State>& seed, const std::vector<Mesh>& meshes, float crossover = 0.8803f, float weight = 0.4717f)
 {
 	std::vector<State> result(seed.size());
 
@@ -424,13 +663,43 @@ void dump_stats(const State& state, const std::vector<Mesh>& meshes)
 	printf("\n");
 }
 
+enum Algorithm
+{
+	GA,
+	SA,
+	DE
+};
+
 int main(int argc, char** argv)
 {
-	std::vector<Mesh> meshes;
+	Algorithm algorithm = DE;
+	int seeds = 0;
 
+	if (strcmp(argv[1], "GA") == 0)
+	{
+		algorithm = GA;
+		seeds = 1000;
+	}
+	else if (strcmp(argv[1], "SA") == 0)
+	{
+		algorithm = SA;
+		seeds = 32;
+	}
+	else if (strcmp(argv[1], "DE") == 0)
+	{
+		algorithm = DE;
+		seeds = 95;
+	}
+	else
+	{
+		printf("Error: need to specify GA/SA/DE as first argument\n");
+		return 1;
+	}
+
+	std::vector<Mesh> meshes;
 	meshes.push_back(gridmesh(50));
 
-	for (int i = 1; i < argc; ++i)
+	for (int i = 2; i < argc; ++i)
 		meshes.push_back(objmesh(argv[i]));
 
 	size_t total_triangles = 0;
@@ -451,24 +720,40 @@ int main(int argc, char** argv)
 	}
 	else
 	{
-		pop = gen0(95, meshes);
+		pop = gen0(seeds, meshes);
 	}
 
 	printf("%d meshes, %.1fM triangles\n", int(meshes.size()), double(total_triangles) / 1e6);
 
 	for (;;)
 	{
-		auto best = genN(pop, meshes);
+		std::pair<State, float> best;
+
+		switch (algorithm)
+		{
+		case GA:
+			best = genN_GA(pop, meshes, 0.7f, 0.3f);
+			break;
+
+		case SA:
+			best = genN_SA(pop, meshes, 31);
+			break;
+
+		case DE:
+			best = genN_DE(pop, meshes);
+			break;
+		}
+
 		gen++;
 
 		if (gen % 10 == 0)
 		{
-			printf("%d: fitness %f;", int(gen), best.second);
+			printf("%s: %d: fitness %f;", argv[1], int(gen), best.second);
 			dump_stats(best.first, meshes);
 		}
 		else
 		{
-			printf("%d: fitness %f\n", int(gen), best.second);
+			printf("%s: %d: fitness %f\n", argv[1], int(gen), best.second);
 		}
 
 		dump_state(best.first);
